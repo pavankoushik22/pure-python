d4e724b86142b10630a00c86c6da45c7813c49bc
pk <pavankoushik22@gmail.com>
2017-08-10 18:36:35 +0530

    2.Implementation of insertion , append, push

--- linked-lists/linked_lists_implementation.py
+++ linked-lists/linked_lists_implementation.py
@@ -13,6 +13,29 @@ class LinkedList:
         while temp:
             print(temp.data)
             temp = temp.next
+    # inserting at the starting of the linked list
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+    # inserting after a given node
+    def insert_after(self, prev_node, new_data):
+        if prev_node is None:
+            print('given node should be in the linked list')
+            return
+        new_node = Node(new_data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+    # adding node at the end of the linked list
+    def append(self, new_data):
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
 
 if __name__ == '__main__':
     llist = LinkedList()
@@ -21,4 +44,7 @@ if __name__ == '__main__':
     third = Node(3)
     llist.head.next = second
     second.next = third
+    llist.append(4)
+    llist.push(5)
+    llist.insert_after(second, 7)
     llist.print_list()
